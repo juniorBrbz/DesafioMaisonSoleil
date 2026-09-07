3b11fd64271553ecaaade2a0abc1c4a7cb0ef9ce
juniorBrbz <juniiiur@gmail.com>
2026-09-06 21:00:50 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('.btn');
-const show = document.getElementById ('nav');
-const icon = document.querySelector('.btn i');
-
-btn.addEventListener('click', () => {
-    show.classList.toggle('show');
-    icon.classList.toggle('bi-x');
-    icon.classList.toggle('bi-list');
-})
